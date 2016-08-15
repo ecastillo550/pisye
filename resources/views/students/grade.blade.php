65ba778c999e4f124f892b21134e60315d630db5
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-08-15 03:32:51 -0500

    se agrega archivo de calificar

---
+++ resources/views/students/grade.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Clase: {{ $class->name }}</div>
+
+                <div class="panel-body">
+                    <form method="post" action="{{ route('students.grade', [$student->id, $class->id]) }}">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <table class="table table-striped">
+                        <tr>
+                            <td>Parcial 1</td>
+                            <td>
+                                <input type="number" min="0" max="100" name="grade1" value="">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Parcial 2</td>
+                            <td>
+                                <input type="number" min="0" max="100" name="grade2" value="">
+                            </td>
+                        </tr>
+                    </table>
+                    <input type="submit" class="btn btn-primary" value="Guardar">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
