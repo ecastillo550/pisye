5ff76589dc47a8aa36c264aece375dececb4a4b0
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-08-30 15:57:06 -0500

    better grade select

--- resources/views/teacher/class.blade.php
+++ resources/views/teacher/class.blade.php
@@ -21,7 +21,11 @@
                         <tr>
                             <td>{{ $student->name }}</td>
                             @foreach($class->semester->partials->sortBy('order') as $partial)
-                                <td><a href="{{ route('students.grade.partial', [$student->id, $class->id, $partial->id]) }}">{{ $student->grades->find($class)->where('partial_id', $partial->order)->first()->cuantitative or 0}}</a></td>
+                                <td><a href="{{ route('students.grade.partial', [$student->id, $class->id, $partial->id]) }}">
+                                {{ !empty($student->grades) 
+                                && !empty($student->grades->where('class_id', $class->id)->where('partial_id', $partial->order)->first()) 
+                                ? $student->grades->where('class_id', $class->id)->where('partial_id', $partial->order)->first()->cuantitative 
+                                : 0 }}</a></td>
                             @endforeach
                             <td><a class="btn btn-primary" href="{{ route('students.grade', [$student->id, $class->id]) }}">Calificar</a></td>
                         </tr>
