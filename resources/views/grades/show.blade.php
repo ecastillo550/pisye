e2777b846ac0c36f7e054df06443e5d8376546e4
Erick Castillo de la Garza <erick@hagane.io>
2018-05-25 13:51:23 -0500

    desplegar observaciones

--- resources/views/grades/show.blade.php
+++ resources/views/grades/show.blade.php
@@ -20,4 +20,23 @@
     </div>
   </div>
 </div>
+
+<div class="card">
+  <div class="card-header">
+    <h1>Observaciones</h1>
+  </div>
+  <div class="card-body">
+    <p>
+      @foreach ($student->enrolled as $group)
+        @foreach($group->semester->partials->sortBy('order') as $partial)
+          {{ !empty($student->grades)
+          && !empty($student->grades->where('group_id', $group->id)->where('partial_id', $partial->id)->first())
+          && !empty($student->grades->where('group_id', $group->id)->where('partial_id', $partial->id)->first()->comments)
+          ? $group->subject->name . ', ' . $partial->name . ': ' . $student->grades->where('group_id', $group->id)->where('partial_id', $partial->id)->first()->comments
+          : null }}
+        @endforeach
+      @endforeach
+    </p>
+  </div>
+</div>
 @endsection
